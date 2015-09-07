f57ba8455891775750626802546be298877b5987
randomin <min427@gmail.com>
2015-09-08 00:59:54 +0900

    Gives a resume site prototype.

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
-(function() {
-  var app = angular.module('app', []);
+(function () {
 
-  app.controller('Ctrl', function($scope) {
-    this.setMe = function() {
-      console.log('This is the test');
-    };
+'use strict';
+
+var querySelector = document.querySelector.bind(document);
+var ctn = querySelector('.ctn-curtain');
+
+var app = angular.module('app', ['firebase']);
+
+app.controller('MainCtrl', function($scope, $element, $firebaseObject) {
+  var ref = new Firebase('https://popping-heat-9561.firebaseio.com/');
+  this.data = $firebaseObject(ref);
+
+  this.sayHello = function() {
+    console.log(this.data.user);
+  };
+});
+
+app.controller('StickyCtrl', function($scope, $element, $window) {
+  var scope = $scope;
+  var element = $element;
+
+  var init = function() {
+
+  };
+
+  angular.element($window).bind('scroll', function() {
+    console.log('test');
   });
+});
 
-  app.directive('testDirective', function() {
-    return {
-      controller: 'Ctrl',
-      controllerAs: 'ctrl',
-      template: 'Test',
-      link: function(scope, element, attrs, ctrl) {
-
+app.directive('sticky', function() {
+  return {
+    restrict: 'C',
+    controller: 'MainCtrl',
+    compile: function(tElement, tAttrs, transclude) {
+      return {
+        pre: function preLink(scope, element, attrs, ctrl) {
+          element.wrap('<div class="sticky-nav"></div>');
+          var wrapper = element.parent('sticky-nav');
+          wrapper[0].style.height = element[0].offsetHeight + 'px';
+        }
       }
     }
-  });
+  }
+});
+
 })()
