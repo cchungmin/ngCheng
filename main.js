230c728adf6b0f5de4c5fd0b1610484c1a29d32b
Chung-Min Cheng <cchungmin@google.com>
2015-09-08 13:24:41 +0900

    Initial verison of sticky nav

--- main.js
+++ main.js
@@ -7,32 +7,31 @@ var ctn = querySelector('.ctn-curtain');
 
 var app = angular.module('app', ['firebase']);
 
-app.controller('MainCtrl', function($scope, $element, $firebaseObject) {
-  var ref = new Firebase('https://popping-heat-9561.firebaseio.com/');
-  this.data = $firebaseObject(ref);
+app.controller('MainCtrl', function($scope, $element, firebase) {
+  this.data = firebase.getFirebaseData();
 
   this.sayHello = function() {
     console.log(this.data.user);
   };
 });
 
-app.controller('StickyCtrl', function($scope, $element, $window) {
+app.controller('StickyCtrl', function($scope, $element, $window, firebase) {
   var scope = $scope;
   var element = $element;
+  this.data = firebase.getFirebaseData();
 
   var doc = document.documentElement;
   var body = document.body
   var target = document.querySelector('.profile');
-  var parent = element.parent();
   var targetOffset = target.offsetTop;
 
   angular.element($window).bind('scroll', function() {
-    var docHeight = doc.scrollTop || body.scrollTop;
+    var docHeight = doc && doc.scrollTop || body && body.scrollTop;
 
     if (docHeight > targetOffset) {
-      parent.addClass('active');
+      scope.$emit('addSticky');
     } else {
-      parent.removeClass('active');
+      scope.$emit('removeSticky');
     }
   });
 });
@@ -40,17 +39,36 @@ app.controller('StickyCtrl', function($scope, $element, $window) {
 app.directive('sticky', function() {
   return {
     restrict: 'C',
-    controller: 'MainCtrl',
+    controller: 'StickyCtrl',
     compile: function(tElement, tAttrs, transclude) {
       return {
         pre: function preLink(scope, element, attrs, ctrl) {
-          element.wrap('<div class="sticky-nav"></div>');
-          var wrapper = element.parent('sticky-nav');
-          wrapper[0].style.height = element[0].offsetHeight + 'px';
+          element.wrap('<div class="sticky-nav container-fluid"></div>');
+          var parent = element.parent();
+          scope.$on('addSticky', function() {
+            if (!parent.hasClass('active')) {
+              element.parent().addClass('active');
+            }
+          });
+
+          scope.$on('removeSticky', function() {
+            if (parent.hasClass('active')) {
+              element.parent().removeClass('active');
+            }
+          });
         }
       }
     }
   }
+});
+
+app.service('firebase', function($firebaseObject) {
+  var ref = new Firebase('https://popping-heat-9561.firebaseio.com/');
+  this.firebaseData = $firebaseObject(ref);
+
+  this.getFirebaseData = function() {
+    return this.firebaseData
+  };
 });
 
 })()
